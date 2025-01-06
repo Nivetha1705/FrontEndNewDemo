4ed128ebf7238cd9fd9b47256d4b5cffd2b6ce89
NIVETHA S <149925944+Nivetha1705@users.noreply.github.com>
2025-01-06 15:16:03 +0530

    Delete src directory

--- src/contactus.jsx
+++
@@ -1,8 +0,0 @@
-function Contactus(){
-    return(
-        <div>
-           <h1>Contactus if any help needed at anytime📲</h1> 
-        </div>
-    )
-}
-export default Contactus;
--- src/home.jsx
+++
@@ -1,9 +0,0 @@
-function Home(){
-    return(
-        <div>
-            <h1>Welcome to Home Page🏡</h1>
-        </div>
-
-    )
-}
-export default Home;
